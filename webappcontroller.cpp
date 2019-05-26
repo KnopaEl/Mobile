6653af7c84d4eab5737c4968f9acd29d558e2bac
Elizabeth <43093955+KnopaEl@users.noreply.github.com>
2019-05-26 19:00:53 +0300

    Сделана 4 лаба

--- webappcontroller.cpp
+++ webappcontroller.cpp
@@ -4,21 +4,49 @@
 #include <QNetworkReply>
 #include <QUrl>
 
-WebAppController::WebAppController(QObject *parent) : QObject(parent)
+WebAppController::WebAppController(QObject *QMLObject) : poisk(QMLObject)
 {
-    manager = new QNetworkAccessManager(this);
-    connect(manager, &QNetworkAccessManager::finished, this, &WebAppController::onRezult);
+    manager = new QNetworkAccessManager(this); // создание менеджера, который будет отправлять запросы
+    connect(manager, &QNetworkAccessManager::finished, this, &WebAppController::onPageInfo);
 
 }
 
 void WebAppController::getPageInfo()
 {
-    manager->get(QNetworkRequest(QUrl("https://yandex.ru/pogoda/moscow/details#1")));
+    manager->get(QNetworkRequest(QUrl("https://yandex.ru/pogoda/moscow")));
 }
 
-void WebAppController::onRezult(QNetworkReply *reply)
+void WebAppController::onPageInfo(QNetworkReply *reply)
 {
     qDebug()<<reply->url();
     qDebug()<<reply->rawHeaderList();
     //qDebug()<<reply->readAll(); - не станет выводить, так как другая кодировка
+
+    // Если запрос не отправляется, то выводим ошибку в Debug
+    if(reply->error()){
+        qDebug() << "ERROR";
+        qDebug() << reply->errorString();
+        }
+    else { // если без ошибок
+        QString pogoda = reply -> readAll();// В переменную передается код страницы HTML
+
+        QObject* text_edit = poisk->findChild<QObject*>("text_edit"); // находим элемент text_edit из qml-кода
+
+        QObject* otbr = poisk->findChild<QObject*>("otbr"); // находим элемент text_edit из qml-кода
+
+        otbr -> setProperty("text", pogoda);
+
+        int i = 0;
+        if ((i= pogoda.indexOf("temp__value", i)) != -1)
+        {
+            qDebug() << "\n" << "Погода на сегодняшний день" << i;
+            qDebug() << pogoda.mid(i+13,3); //пропускаем 13 символа после нашего тега и выводим следующие 3, например, +21
+
+            text_edit -> setProperty("text", pogoda.mid(i+13,3));
+        }
+    }
+}
+
+void WebAppController::onAuth(QString login, QString password){
+
 }
