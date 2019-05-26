56d601cfcc8aee9a969ccab2cdcd9643fd699816
Elizabeth <43093955+KnopaEl@users.noreply.github.com>
2019-05-26 19:02:13 +0300

    Изменения с учетом 4 лабы

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
+#include <QQmlContext>
 #include "webappcontroller.h"
 
 int main(int argc, char *argv[])
@@ -13,6 +14,13 @@ int main(int argc, char *argv[])
     if (engine.rootObjects().isEmpty())
         return -1;
     WebAppController wa;
-    wa.getPageInfo();
+    //wa.getPageInfo();
+
+//    QObject::connect(engine.rootObjects().first(), SIGNAL(getPageInfo()), &wa, SLOT(getPageInfo()));
+    //связь C++ и Qml:
+    QObject* root = engine.rootObjects()[0];
+    WebAppController myV(root);
+    engine.rootContext()->setContextProperty("_myV", &myV);
+
     return app.exec();
 }
