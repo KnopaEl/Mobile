7f14e12a059f5cdb6a732c27022df812694ccf64
Elizabeth <43093955+KnopaEl@users.noreply.github.com>
2019-05-29 23:07:20 +0300

    Сделана 5 лаба

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include "webappcontroller.h"
-
+#include <QNetworkReply>
 int main(int argc, char *argv[])
 {
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
@@ -16,11 +16,20 @@ int main(int argc, char *argv[])
     WebAppController wa;
     //wa.getPageInfo();
 
+    QObject::connect(engine.rootObjects().first(), SIGNAL(success(QString)),
+    &wa, SLOT(success(QString)));
+    QObject::connect(engine.rootObjects().first(), SIGNAL(onAuth(QString, QString)),
+    &wa, SLOT(onAuth(QString, QString)));
+    QObject::connect(engine.rootObjects().first(), SIGNAL(restRequest()),
+    &wa, SLOT(restRequest()));
+
+
 //    QObject::connect(engine.rootObjects().first(), SIGNAL(getPageInfo()), &wa, SLOT(getPageInfo()));
     //связь C++ и Qml:
     QObject* root = engine.rootObjects()[0];
     WebAppController myV(root);
     engine.rootContext()->setContextProperty("_myV", &myV);
+
 
     return app.exec();
 }
